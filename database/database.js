b6d626949c0f0c984bf45811a0017c7036ba2e45
FLAVIO GUILHERME DOS SANTOS <81536682+flavioguisantos@users.noreply.github.com>
2021-05-12 01:45:12 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize('guiapress', 'guilherme', 'ctrl', {
+const connection = new Sequelize('guiapress', 'guiapress', 'guiapress', {
     host: 'localhost',
     dialect: 'mysql',
     timezone: "-03:00"
